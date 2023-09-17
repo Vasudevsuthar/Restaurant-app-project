e0541a5ca682132edf827a6189fb0ea1c8929725
Vasudevsuthar <133574340+Vasudevsuthar@users.noreply.github.com>
2023-09-17 23:13:12 +0530

    Add files via upload

---
+++ Restaurant app/src/component/Layout/Header.js
@@ -0,0 +1,21 @@
+import { Fragment } from "react";
+
+import HeaderCartButton from "./HeaderCartButton";
+import image from '../../assets/images.jpg'
+import classes from './Header.module.css'
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Khana Khazana</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={image} alt="A table full of delicious food"/>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
