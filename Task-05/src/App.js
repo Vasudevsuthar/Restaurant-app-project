9c37c92dfb2cdc1e6e368e5b205d3f1fb60aa225
Vasudevsuthar <133574340+Vasudevsuthar@users.noreply.github.com>
2023-09-20 12:05:21 +0530

    Add files via upload

---
+++ Task-05/src/App.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+
+import Header from "./component/Layout/Header";
+import Meals from "./component/Meals/Meals";
+import Cart from "./component/Cart/Cart";
+
+function App() {
+  return (
+    <Fragment>
+      <Cart />
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
